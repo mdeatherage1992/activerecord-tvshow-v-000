4701f25fc2f276dc5ea664376375cb9ada85aa7f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-09 13:10:47 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,29 @@
 class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum("number_of_stars")
+  end
+
+  def most_popular_show
+    Show.where("number_of_stars > ?", 10)
+  end
+
+  def lowest_rating
+    Show.minimum(:number_of_stars)
+  end
+
+  def least_popular_show
+    Show.minimum(:number_of_stars)
+  end
+
+  def ratings_sum
+    Show.sum(:number_of_stars)
+  end
+
+  def popular_shows
+    Show.where("number_of_stars > ?", 5)
+  end
+
+  def shows_by_alphabetical_order
+    Show.order(:name)
+  end
 end
